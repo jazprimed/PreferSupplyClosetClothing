43bf05dedfba48d0869645e48425fa33d13217ad
jazprimed <jazprimed@gmail.com>
2026-02-28 11:53:59 +1000

    Rename mod to Outfit Override

--- OutfitOverride/OutfitOverride.cs
+++ OutfitOverride/OutfitOverride.cs
@@ -11,37 +11,12 @@ namespace PreferSupplyClosetClothing
         static WearableAccessorizer.WearableType Postfix(WearableAccessorizer.WearableType __result, WearableAccessorizer __instance)
         {
             Dictionary<WearableAccessorizer.WearableType, WearableAccessorizer.Wearable> wearables = Traverse.Create(__instance).Field("wearables").GetValue<Dictionary<WearableAccessorizer.WearableType, WearableAccessorizer.Wearable>>();
-
-            Debug.Log("__GetHighestAccessory__");
-            Debug.Log("Dupe: " + __instance.GetProperName());
-            Debug.Log("initial result?: " + __result.ToString());
-
-
-            foreach (ClothingOutfitUtility.OutfitType key in __instance.GetCustomClothingItems().Keys)
-            {
-                Debug.Log("clothing types: " + key.ToString());
-                for (int i = 0; i < __instance.GetCustomClothingItems()[key].Count; i++)
-                {
-                    Debug.Log("GetCustomClothingItems: " + __instance.GetCustomClothingItems()[key][i].Get().Name);
-                }
-            }
-
-
-            foreach (WearableAccessorizer.WearableType key in wearables.Keys)
-            {
-                Debug.Log("wearables: " + key.ToString());
-                Wearable value = wearables[key];
-                Debug.Log("buildOverridePriority: " + value.buildOverridePriority);
-            }
-
             if (__result == WearableAccessorizer.WearableType.Outfit & wearables.Keys.ToList().Contains(WearableAccessorizer.WearableType.CustomClothing))
             {
-                Debug.Log("return CustomClothing");
                 return WearableAccessorizer.WearableType.CustomClothing;
             }
             else
             {
-                Debug.Log("return " + __result.ToString());
                 return __result;
             }
         }
@@ -53,22 +28,15 @@ namespace PreferSupplyClosetClothing
     {
         static void Prefix(WearableAccessorizer __instance)
         {
-
-            Debug.Log("__ApplyWearable__");
-            Debug.Log("GetProperName: " + __instance.GetProperName());
-
             Dictionary<WearableAccessorizer.WearableType, WearableAccessorizer.Wearable> wearables = Traverse.Create(__instance).Field("wearables").GetValue<Dictionary<WearableAccessorizer.WearableType, WearableAccessorizer.Wearable>>();
             foreach (WearableAccessorizer.WearableType key in wearables.Keys)
             {
-                Debug.Log("wearables: " + key.ToString());
                 Wearable value = wearables[key];
-                Debug.Log("buildOverridePriority: " + value.buildOverridePriority);
                 if (key == WearableAccessorizer.WearableType.CustomClothing)
                 {
                     value.buildOverridePriority = 6;
                 }
             }
-
         }
     }
 }
